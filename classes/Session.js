a4e2c0e7d906b305c96afd057420a02db4af9221
Michael Delk <michael@delkdev.com>
2022-11-30 22:32:26 -0700

    Session object methods

--- classes/Session.js
+++ classes/Session.js
@@ -1,9 +1,15 @@
 class Session {
     id = 0;
+    exercises = [];
 
     constructor(sessionId) {
         this.id = sessionId;
     }
+
+    addExercise(exercise) {
+        this.exercises.push(exercise);
+    }
+
 };
 
 module.exports = Session;
--- index.js
+++ index.js
@@ -1,20 +1,29 @@
+/**
+ * index.js
+ * 
+ * Calculate warmup sets, reps and weights for strength training programs like 
+ * Starting Strength.
+ * 
+ * @package lifter
+ * @author Michael Delk
+ * @version 2022-11-29
+ */
+
 fs = require('fs');
 Cycle = require('./classes/Cycle.js');
 Session = require('./classes/Session.js');
 
+/**
+ * Equipment: Assume non-metric lbs.
+ */
 const equipment = {
     metric: false,
     barWeight: 45,
     weights: [45, 35, 25, 10, 5, 2.5]
 };
 
-const programFilePath = './programs/texas-intermediate-5x5.json';
-const program = JSON.parse(
-        fs.readFileSync(programFilePath, {encoding: "utf8"})
-    );
-
 /**
- * Load exercises for Texas Intermediate Program.
+ * Exercises: Descriptions, sets, reps, and percentages.
  */
 const exercisesFilePath = './programs/lifter-exercises.json';
 const exercises = JSON.parse(
@@ -45,13 +54,16 @@ const beg5RMs = {
 }
 
 cycles = [];
-cyclesToGenerate = 3;
+cyclesToGenerate = 1;
 
 for (let cycleId = 1; cycleId <= cyclesToGenerate; cycleId++) {
     const cycle = new Cycle(cycleId, beg5RMs);
 
     for (let sessionId = 1; sessionId <= 6; sessionId++) {
         const session = new Session(sessionId);
+        session.addExercise(backSquat);
+        session.addExercise(benchPress);
+        session.addExercise(deadlift);
         cycle.addSession(session);
     }
     
@@ -59,3 +71,4 @@ for (let cycleId = 1; cycleId <= cyclesToGenerate; cycleId++) {
 }
 
 console.info(cycles);
+console.info(JSON.stringify(cycles));
